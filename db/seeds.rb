3e27abe85a1de3610f9032236e21a0011ff7f047
sato-ayumi <129816397+sato-ayumi@users.noreply.github.com>
2023-06-23 08:53:54 +0000

    [Fix] seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,25 +10,25 @@ Admin.create!(
   password: "aaaaaa"
 )
 
-# User.create!(
-#   [
-#     {
-#       nickname: "ナンバ",
-#       email: "0@0",
-#       is_deleted: false,
-#       password: "000000",
-#     },
-#     {
-#       nickname: "シンジュク",
-#       email: "1@1",
-#       is_deleted: false,
-#       password: "111111",
-#     },
-#     {
-#       nickname: "ロッポンギ",
-#       email: "2@2",
-#       is_deleted: false,
-#       password: "222222",
-#     }
-#   ]
-# )
+User.create!(
+  [
+    {
+      nickname: "ナンバ",
+      email: "0@0",
+      is_deleted: false,
+      password: "000000",
+    },
+    {
+      nickname: "シンジュク",
+      email: "1@1",
+      is_deleted: false,
+      password: "111111",
+    },
+    {
+      nickname: "ロッポンギ",
+      email: "2@2",
+      is_deleted: false,
+      password: "222222",
+    }
+  ]
+)
